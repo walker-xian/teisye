d48127b57e8ba425329271488c0604fcb4c5f7cf
Daiqian Huang <daiqian.huang@outlook.com>
2019-07-30 11:34:06 +0800

    run once is good enough.

--- scripts/go.sh
+++ scripts/go.sh
@@ -4,7 +4,7 @@ for thread in 1 2 4 8
 do
     for heap in 0 1 
     do
-        for ((i = 0; i < 3; ++i)) 
+        for ((i = 0; i < 1; ++i)) 
         do
             $heapperf -c 15 -l 3 -t $thread $heap
         done
